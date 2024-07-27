07509dd0fea1c8e90eb86a0aeb96fab3aa1706f8
rachitsgh <rachit2351@gmail.com>
2024-07-28 00:55:07 +0530

    added middleware

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,25 @@
-/* create routes that access the tokenizer() and parse() functions */
+// server.js or app.js
+const express = require('express');
+const connectDB = require('./database/config');
+const router = require('./routes/ruleRoutes')
 
+const app = express();
+
+// Load environment variables
+// require('dotenv').config();
+
+// Connect to MongoDB
+connectDB();
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.use('/api/rules', router);
+
+const PORT = 5004;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
