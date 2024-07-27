f6ea594aa7502bd176f3e3737b652da4a235a81a
rachitsgh <rachit2351@gmail.com>
2024-07-28 00:54:31 +0530

    {rule Schema}

---
+++ backend/models/ruleSchema.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ruleSchema = new Schema({
+    ruleName: String,
+    root: { type: Schema.Types.ObjectId, ref: 'Node' },
+    postfixExpr: [String]
+});
+
+module.exports = mongoose.model('Rule', ruleSchema);
