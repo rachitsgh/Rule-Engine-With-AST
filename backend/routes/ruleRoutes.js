38e4f217e4a223046aa0595e95269cc862d27680
rachitsgh <rachit2351@gmail.com>
2024-07-28 00:52:19 +0530

    fixed typo error

--- backend/routes/ruleRoutes.js
+++ backend/routes/ruleRoutes.js
@@ -1,14 +1,16 @@
 const express=require('express');
 const router=express.Router();
-const createRule=require('../controllers/createRule')
-const combineRules=require('../controllers/combineRule')
-const evaluateRule=require('../controllers/evaluateRule')
+const {createRule}=require('../controllers/createRule')
+// const {combineRules}=require('../controllers/combineRule')
+// const {evaluateRule}=require('../controllers/evaluateRule')
 
 
-//TODO
+
 //createRule
-router.post('/create_rule',createRule);
-router.post('/combine_rules',combineRules);
-router.post('/evaluate_rule',evaluateRule);
+router.post('/create',createRule);
+//combineRule
+// router.post('/combine_rules',combineRules);
+//evaluateRule
+// router.get('/evaluate_rule',evaluateRule);
 
-module.exports={router}
+module.exports=router
