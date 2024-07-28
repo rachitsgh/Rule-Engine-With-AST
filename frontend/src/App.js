03de4245aa2302b8758703114ec7313a9acc1fe8
rachitsgh <rachit2351@gmail.com>
2024-07-28 19:09:27 +0530

    Added Components and did routing'

---
+++ frontend/src/App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import CreateRule from './components/Create_Rule';
+// import CombineRules from './components/CombineRules';
+import EvaluateRule from './components/Evaluateule';
+import { ToastContainer } from 'react-toastify';
+
+function App() {
+
+
+  return (
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/create-rule" element={<CreateRule />} />
+          {/* <Route path="/combine-rules" element={<CombineRules />} /> */}
+          <Route path="/evaluate-rule" element={<EvaluateRule />} />
+        </Routes>
+        <ToastContainer
+          position="top-right"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="colored"/>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
