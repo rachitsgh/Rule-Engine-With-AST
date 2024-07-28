2a8380944f8cfabe77b25020017009b1458e05d5
rachitsgh <rachit2351@gmail.com>
2024-07-28 21:45:46 +0530

    adde routes for combining rules

--- backend/routes/ruleRoutes.js
+++ backend/routes/ruleRoutes.js
@@ -1,7 +1,7 @@
 const express=require('express');
 const router=express.Router();
 const {createRule}=require('../controllers/createRule')
-// const {combineRules}=require('../controllers/combineRule')
+const {combineRules}=require('../controllers/combineRule')
 const {evaluateRule}=require('../controllers/evaluateRule');
 const getAllRules = require('../controllers/rules');
 
@@ -10,7 +10,7 @@ const getAllRules = require('../controllers/rules');
 //createRule
 router.post('/create',createRule);
 //combineRule
-// router.post('/combine_rules',combineRules);
+router.post('/combine_rules',combineRules);
 //evaluateRule
 router.post('/eval',evaluateRule);
 //getAllRules
